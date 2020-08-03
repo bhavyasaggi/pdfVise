d5e8960b36aa91c38115d12c54dca61e40edb2b4
bhavyasaggi <bhavya.saggi@gmail.com>
2020-08-03 22:15:32 +0530

    +PWA_offline

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,23 @@
 'use strict';
 var cacheName = 'pdfVise';
 var filesToCache = [
+  "{{ "/favicon.ico" | absolute_url }}",
+  "{{ "/404.html" | absolute_url }}",  
+  {%- for page_type in site.data.type -%}
+  "{{ page_type[1].link | absolute_url }}",
+  {%- endfor -%}
+  "{{ "/assets/js/combine.js" | absolute_url }}",
+  "{{ "/assets/js/merge.js" | absolute_url }}",
+  "{{ "/assets/js/sort.js" | absolute_url }}",
+  "{{ "/assets/css/action.css" | absolute_url }}",
   "{{ "/assets/images/logo.jpg" | absolute_url }}",
+  "{{ "/assets/icons/move.svg" | absolute_url}}",
+  "{{ "/assets/icons/rotate-cw.svg" | absolute_url}}",
+  "{{ "/assets/icons/rotate-ccw.svg" | absolute_url}}",
+  "{{ "/assets/icons/trash.svg" | absolute_url}}",
+  "{{ "/assets/icons/trash-2.svg" | absolute_url}}",
+  "{{ "/assets/site.webmanifest" | absolute_url }}",
+  "https://unpkg.com/pdfjs-dist@latest/build/pdf.worker.min.js",
   "https://unpkg.com/pdfjs-dist@latest/build/pdf.min.js",
   "https://unpkg.com/pdf-lib@latest/dist/pdf-lib.min.js",
   "https://unpkg.com/sortablejs@latest/Sortable.min.js",
@@ -39,19 +55,23 @@ self.addEventListener('activate', function (e) {
 // Fetch
 self.addEventListener('fetch', function(e) {
   if (!(
+      // For POST requests, do not use the cache. Serve offline page if offline.
+      (e.request.method === 'GET') &&
+      // Return if request url protocal isn't http or https
+      e.request.url.match(/^(http|https):\/\//i) &&
       // Return if the current request url is in the never cache list
       neverCacheUrls.every(function (url) {
         return this.match(url) ? false : true;
-      }, e.request.url) &&
-      // Return if request url protocal isn't http or https
-      e.request.url.match(/^(http|https):\/\//i) &&
-      // Return if request url is from an external domain.
-      (new URL(e.request.url).origin === location.origin) &&
-      // For POST requests, do not use the cache. Serve offline page if offline.
-      (e.request.method === 'GET')
-    )) {
+      }, e.request.url)
+  )) {
     return;
   }
+  if(
+    !~filesToCache.indexOf(e.request.url) &&
+    new URL(e.request.url).origin !== location.origin
+  ) {
+    return;
+  }  
   e.respondWith(
     // Network First
     fetch(e.request).then(function(fetchRequest) {
